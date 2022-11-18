5104a605a5d923e2da917f5819bbf023d5c66dd0
DESKTOP-FGGBIP3\Le Bouedec Alex <alex.lebouedec@gmail.com>
2022-11-18 15:16:16 +0100

    setup navigation

--- app/src/main/java/com/example/exoweather/common/presentation/MainActivity.kt
+++ app/src/main/java/com/example/exoweather/common/presentation/MainActivity.kt
@@ -6,38 +6,38 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.exoweather.common.presentation.screen.Screen
 import com.example.exoweather.common.presentation.ui.theme.ExoWeatherTheme
+import com.example.exoweather.feature.home.presentation.HomeScreen
+import com.example.exoweather.feature.weather.presentation.WeatherScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ExoWeatherTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.Home.route
+                    ) {
+                        composable(route = Screen.Home.route) {
+                            HomeScreen(navController = navController)
+                        }
+                        composable(route = Screen.Weather.route) {
+                            WeatherScreen(navController = navController)
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ExoWeatherTheme {
-        Greeting("Android")
     }
 }
